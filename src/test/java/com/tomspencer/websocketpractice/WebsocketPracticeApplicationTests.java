59118e8a62a871243d21c52bdcc6aac81f574ca0
Tom Spencer <thomas.spencer@codurance.com>
2022-08-13 14:19:16 +0100

    add websocket practice

---
+++ src/test/java/com/tomspencer/websocketpractice/WebsocketPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomspencer.websocketpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketPracticeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
